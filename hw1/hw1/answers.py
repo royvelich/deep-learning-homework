d9e42a1c56426c2d2785768d690bb683e6d3f8ef
Roy Velich <xenomarz@users.noreply.github.com>
2019-11-25 21:51:31 +0200

    Update answers.py

--- hw1/hw1/answers.py
+++ hw1/hw1/answers.py
@@ -12,12 +12,12 @@ part2_q1 = r"""
 **Your answer:**
 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+Let's look at the two extreme cases:
+
+ - When k=1, we get complete overfitting to the training set.
+ - When k="number of samples in training set", the overfitting is reduced to minimum, the model will return the same class for all unseen data.
+
+This suggest that typical k should not be too close to one of the extreme points, and is probably depends on the density of the traning set, which in turn should be similar to the density of the true distribution. That's why in practice we use cross-validation to determine k.
 
 """
 
@@ -25,12 +25,8 @@ part2_q2 = r"""
 **Your answer:**
 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+ 1. This method will select the model which best overfits the whole training data, while in cross-validation we only overfit a portion of the training data.
+ 2. Since test set is unlabeled data, it is impossible to calculate its accuracy.
 
 """
 
