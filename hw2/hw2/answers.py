8ef314c99bd94790d63e4496f393d1f9edded32e
Roy Velich <xenomarz@users.noreply.github.com>
2019-12-22 00:43:45 +0200

    Update answers.py

--- hw2/hw2/answers.py
+++ hw2/hw2/answers.py
@@ -79,66 +79,38 @@ An equation: $e^{i\pi} -1 = 0$
 
 part3_q1 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+  1. We can clearly see that as the depth deepens the accuracy drops. The depth that produced the best
+results is L=4 K=64, and this is because the gradients are still viable at this depth.
+  2. At L=16 the network wasn't trainable at all (loss stuck at constant level). The cause for this is the phenomena
+of vanishing gradients. To resolve this issue, we can use either 'skip-connections' to let gradients from deeper
+regions jump downwards, or we can use regularization techniques. 
 
 """
 
 part3_q2 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+We can clearly see that also in this experiment the best results are achieved at L=4 K=64. That means that in simple
+CNN such as ours, increasing the number of filters doesn't compensate for vanishing gradients caused by depth.
 """
 
 part3_q3 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+This experiment emphasis further that for simple CNN the depth plays a major role in the training capability of the
+network, since we use a varying number of filters at each block. We can clearly see that for depths L=2,4 we get very similar
+results, while a sharp drop occurs as the depth increases, up to the point of untraiable network.
 """
 
 part3_q4 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+Here we can clearly see that the 'skip-connections' of the residual blocks are drastically improving the ability of the
+network to be trainable even at very high depths. As we can see from the results, the network is trainable even at L=32.
 """
 
 part3_q5 = r"""
 **Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+  1. Our modifications were to add dropout regularization, batch normalization and use a kernel size of 5.
+  2. We can clearly see that our network is trainable at all configurations and that we have achieved better test
+  accuracy - that is, thanks to regularization, we have prevented overfitting the training dataset, and improved the
+  generalization capabilities of our network. 
 """
 # ==============
